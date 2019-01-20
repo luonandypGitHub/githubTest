700ace28485eda877611535620613a3ae6de9330
luonandypGitHub <45893573+luonandypGitHub@users.noreply.github.com>
2019-01-20 17:43:07 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// $name = $_POST['name'];
+// $age = $_POST['age'];
+// $ginder = $_POST['ginder'];
+$name = "董亚平";
+$age = "21";
+$ginder = "男";
+
+echo json_encode('姓名：'.$name.'年龄：'.$age.'性别：'.$ginder);
+
+// echo "成功";
+
+?>
